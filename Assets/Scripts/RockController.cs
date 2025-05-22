dc585e5c95732ece2ce4e434015dae318a87f62d
John-Rep <replogle.john@next-u.fr>
2025-05-22 11:00:15 -0400

    Ajusté la lumière des cailloux

--- Assets/Scripts/RockController.cs
+++ Assets/Scripts/RockController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class RockController : MonoBehaviour
 {
     GameObject gameManager;
-    float intensityMultiplier = 5f;
+    float intensityMultiplier = 3.5f;
     Rigidbody rb;
     void Start()
     {
